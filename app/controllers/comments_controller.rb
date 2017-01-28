439f93370de215bd807b5b1ba06ab8ef9adb74cf
gatsunoko <gatsunoko@yahoo.co.jp>
2017-01-28 14:57:12 +0900

    コメントの返信機能とルート整理

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,19 @@ class CommentsController < ApplicationController
     redirect_to youtube_video_path(@comment.youtube_video_id)
   end
 
+  def reply
+    #raise.params.inspect
+    unless current_user.nil?
+      @comment = Comment.new
+      @comment.user_id = current_user.id
+      @comment.reply = params[:reply_params][:comment_id].to_i
+      @comment.text = params[:text]
+      @comment.youtube_video_id = params[:reply_params][:youtube_video_id].to_i
+      @comment.save
+    end
+    redirect_to youtube_video_path(params[:reply_params][:youtube_video_id].to_i)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:text, :youtube_video_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :bits do
+  resources :bits, only: [:index] do
     collection do
       get :Search
     end
   end
-  resources :youtube_videos
-  resources :comments
+  resources :youtube_videos, only: [:new, :create, :edit, :update, :show]
+  resources :comments, only: [:create] do
+    collection do
+      post :reply
+    end
+  end
   root 'bits#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
