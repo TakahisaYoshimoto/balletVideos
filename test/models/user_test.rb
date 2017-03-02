6a17c15f0abe4699e90857581234c3b81cf4d25d
kmdtmyk <winback_banderas@yahoo.co.jp>
2017-03-02 21:27:43 +0900

    userのテストを修正

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,13 +3,10 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   test "likeが一緒に消えるテスト" do
-    total_like_count = Like.count
-    root = users(:root)
-    root_like_count = root.likes.count
-    assert_equal 2, root_like_count
-    assert_equal total_like_count, Like.count, "消す前"
-    root.destroy
-    assert_equal total_like_count - root_like_count, Like.count, "消した後"
+    user = users(:level1)
+    assert_not_equal 0 , Like.where(user_id: user.id).count
+    user.destroy
+    assert_equal 0 , Like.where(user_id: user.id).count
   end
 
 end
