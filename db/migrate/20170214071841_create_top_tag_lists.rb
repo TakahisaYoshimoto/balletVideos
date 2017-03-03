605b499dfe30ad2025a4e96b14e956ef8f17eb44
gatsunoko <gatsunoko@yahoo.co.jp>
2017-03-04 07:16:35 +0900

    シェアボタンのURLを環境変数にした

--- db/migrate/20170214071841_create_top_tag_lists.rb
+++ db/migrate/20170214071841_create_top_tag_lists.rb
@@ -1,7 +1,7 @@
 class CreateTopTagLists < ActiveRecord::Migration[5.0]
   def change
     create_table :top_tag_lists do |t|
-      t.string :genre, default: "注目キーワード"
+      t.string :genre
       t.string :tag_name
 
       t.timestamps
