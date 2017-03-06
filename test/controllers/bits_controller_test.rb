698bb0a9b1503bcd24d8e581a61c1f1dee79969b
kmdtmyk <winback_banderas@yahoo.co.jp>
2017-03-06 22:28:26 +0900

    画面項目のテストを追加

--- test/controllers/bits_controller_test.rb
+++ test/controllers/bits_controller_test.rb
@@ -6,20 +6,29 @@ class BitsControllerTest < ActionDispatch::IntegrationTest
     get bits_path
     assert_response :success
     assert_equal 11, assigns(:youtubes).count
+    assert_select '.y_video_img_area', assigns(:youtubes).count
 
     assert_equal 2, assigns(:pic_youtubes).count
     assert_includes assigns(:pic_youtubes), youtube_videos(:pickup_level1)
     assert_includes assigns(:pic_youtubes), youtube_videos(:pickup_level2)
+    assert_select '.y_index_links_pickup_area', 2
   end
 
   test "all" do
     get all_bits_path
     assert_response :success
     assert_equal 12, assigns(:youtubes).count
+    assert_select '.y_video_img_area', assigns(:youtubes).count
 
     get all_bits_path, params: { page: 5 }
     assert_response :success
     assert_equal YoutubeVideo.count % 12, assigns(:youtubes).count, '最後のページは12で割った余り'
+    assert_select '.y_video_img_area', assigns(:youtubes).count
+
+    get all_bits_path, params: { page: 6 }
+    assert_response :success
+    assert_equal 0, assigns(:youtubes).count
+    assert_select '.y_video_img_area', 0
   end
 
 end
