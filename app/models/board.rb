2cd26c8a4462a2c4f5103572dd8af6f485cae5b8
gatsunoko <gatsunoko@yahoo.co.jp>
2017-04-06 14:59:25 +0900

    board_commentのCSSとかちょっと調整

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,5 +2,4 @@ class Board < ApplicationRecord
   belongs_to :user
   has_many :board_comments
   validates :title, presence: true, length: { maximum: 200 }
-  #validates :category, presence: true
 end
