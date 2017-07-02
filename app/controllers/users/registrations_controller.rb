dc0efb6dca4140ea946b3dc095cbc82f85744fcb
gatsunoko <gatsunoko@yahoo.co.jp>
2017-07-02 17:33:46 +0900

    ユーザー登録時redirect先がおかしかった問題修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -27,6 +27,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def after_update_path_for(resource)
     profile_path(current_user.id)
   end
+
+  def after_sign_up_path_for(resource)
+    # admin_user_mail_sent_message_path
+    root_path
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+    # admin_user_mail_sent_message_path
+    sign_up_mail_bits_path
+  end
   # DELETE /resource
   # def destroy
   #   super
